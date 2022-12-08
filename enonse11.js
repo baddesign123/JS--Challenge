60f58dae2a3edab06c0d72c0e3a1634cd5eeb901
baddesign123 <96295111+baddesign123@users.noreply.github.com>
2022-12-08 18:34:28 -0500

    Enonse 11

--- enonse11.js
+++ enonse11.js
@@ -3,7 +3,13 @@ div.setAttribute('style','background:#000; width:300px; height:300px')
 document.body.append(div)
 div.addEventListener('mousemove', function(e){
    let x=logMovement(e)[0], y=logMovement(e)[1]
-   div.style['background']=`rgb(${x}, ${y}, ${parseInt((x+y)/2)})`  
+   let kouleA= parseInt((x+y)/2)
+   if(kouleA>255 || x>255 || y>255 ){
+      kouleA=255
+      x=255
+      y=255
+   }
+   div.style['background']=`rgb(${x}, ${y}, ${kouleA})` 
 })
 
 function logMovement(event) {
@@ -12,4 +18,6 @@ function logMovement(event) {
     let tab=[x, y]
     return tab
   }
+
+
 
